3a27193ed520848143303cac732d5bcee1d63d19
Etienne Brodu <etienne.brodu@datadoghq.com>
2020-07-28 19:10:01 +0200

    :construction: wip

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+# Build the discount-service image if required
+docker build -t discounts-service discounts-service
+
+# Launch the containers
 POSTGRES_USER=postgres POSTGRES_PASSWORD=password docker-compose -p prod up -d
