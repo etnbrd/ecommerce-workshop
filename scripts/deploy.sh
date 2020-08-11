12710bdb50bf680de591701b4a28a2f286a47abb
Etienne Brodu <etienne.brodu@datadoghq.com>
2020-08-11 12:13:09 +0200

    :construction: wip

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,6 +8,7 @@ docker-compose down
 # Launch the containers
 POSTGRES_USER=postgres POSTGRES_PASSWORD=password docker-compose up -d
 
+echo 'waiting for storedog to be ready'
 until $(curl --output /dev/null --silent --head --fail http://0.0.0.0:3000); do
     printf '.'
     sleep 5
