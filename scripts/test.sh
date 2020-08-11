b34302ba27712aa033ac9dddf99e0054c755a3b3
Etienne Brodu <etienne.brodu@datadoghq.com>
2020-08-11 12:37:49 +0200

    :construction: wip

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-if [ -e 'discount.synthetics-ci.json' ]; then
-  datadog-ci synthetics run-tests --apiKey "$DD_API_KEY" --appKey "$DD_APP_KEY"
-fi;
+echo 'test synthetics-CI'
+datadog-ci synthetics run-tests --apiKey "$DD_API_KEY" --appKey "$DD_APP_KEY"
