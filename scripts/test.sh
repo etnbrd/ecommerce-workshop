3a31c39ed011480e163633dd7ba327b526ddb3eb
Etienne Brodu <etienne.brodu@datadoghq.com>
2020-08-11 12:53:03 +0200

    :construction: wip

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo 'test synthetics-CI'
-datadog-ci synthetics run-tests --apiKey "$DD_API_KEY" --appKey "$DD_APP_KEY"
+yarn datadog-ci synthetics run-tests --apiKey "$DD_API_KEY" --appKey "$DD_APP_KEY"
